86239c8ffd54654816fc339ecea1d5ee1e7b8f5f
ydahi <ymdahi@gmail.com>
2019-10-31 14:55:05 -0400

    serve from cached assets

--- sw.js
+++ sw.js
@@ -8,7 +8,8 @@ const assets = [
     '/learningtopwa/css/styles.css',
     '/learningtopwa/css/materialize.min.css',
     '/learningtopwa/img/dish.png',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
 // install event
@@ -16,16 +17,16 @@ self.addEventListener('install', evt => {
     //console.log('sw installed');
 
     // cache assets
-    // putting cacheing in the 'install' event allows
+    // putting caching in the 'install' event allows
     // us to only update caches when sw.js changes
 
-    // because the cacheing is asynchronous and the sw install
-    // might not take that long, we wrap our cacheing in 
+    // because the caching is asynchronous and the sw install
+    // might not take that long, we wrap our caching in 
     // the waitUntil method:
     evt.waitUntil(
         // an asynchronous task that returns a promise:
         caches.open(staticCacheName).then(cache => {
-            console.log('cacheing shell assets')
+            console.log('caching shell assets')
             cache.addAll(assets);
         })
     );
@@ -39,5 +40,14 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
+
+    // check if asset being fetched exists in cache
+    // if so, return cached asset instead of fetching
+    evt.respondWith(
+        // an async that returns a promise:
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request); // return cached response. if empty (||), fetch from server.
+        })
+    );
 
 });
